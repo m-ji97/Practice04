164a0e9521b79e4e4655c8373974ade65f01c070
m-ji97 <tv5035@naver.com>
2024-01-03 18:01:20 +0900

    24-01-03 데이터 추가

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -1,21 +1,13 @@
 package com.javaex.practice;
 
-import java.util.Arrays;
-
 public class Ex02 {
 	public static void main(String[] args) {
-		double[] drr = new double[] {1.2, 3.3, 6.7};
+		double[] num = new double[] {1.2, 3.3, 6.7};
 
-		for(int i = 0; i < (drr.length/2); i++) {
-		    int tmp = drr[i];
-		    drr[i] = drr[drr.length-1-i];
-		 //처음과 마지막 요소 교환
-		    //arr.length-1을 해야 arr의 마지막 요소에 접근한다.
-		    drr[drr.length-i-1] = tmp;
-		}
+		for(int i=0; i<num.length; i++) {
 			
-		    System.out.println(); //확인
 		}
 	}
+}
 
 
---
+++ src/com/javaex/practice/Ex06.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		/*
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		*/
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		char c1[] = {'T','h','i','s',',','i','s',',','a',',','p','e','n','c','i','l'};
+		System.out.println(c);
+		System.out.println(c1);
+		
+	}
+
+}
